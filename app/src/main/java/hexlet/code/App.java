25d8d735eecff9b5c685621e1a9fdef3f75e4695
Eugene <braincucumber@mail.ru>
2024-05-06 17:51:01 +0300

    added templates

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,15 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
 public class App {
@@ -17,6 +24,13 @@ public class App {
         return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
     }
 
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
+
     public static void main(String[] args) {
         var app = getApp();
         app.start(getPort());
@@ -26,22 +40,30 @@ public class App {
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
 
+        String sql = null;
+        try {
+            sql = Files.readString(Paths.get("init.sql"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         try (
                 var dataSource = new HikariDataSource(hikariConfig);
                 var connection = dataSource.getConnection();
-                var statement = connection.createStatement()
+                var statement = connection.createStatement();
         ) {
-            statement.execute("sql");
+            statement.execute(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
         app.get("/", ctx -> {
-            ctx.result("Hello World");
+            ctx.render("index.html");
         });
 
         return app;
